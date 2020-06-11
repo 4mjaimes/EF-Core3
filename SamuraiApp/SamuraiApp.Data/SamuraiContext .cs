efc0b19981d305e00b3b335f4a9bd694a7d9f2d8
Manuel Jaimes <4mjaimes@gmail.com>
2020-06-11 00:11:51 -0500

    End module 2

---
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext .cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\ProjectsV13;Initial Catalog=SamuraiAppData");
+        }
+    }
+}
