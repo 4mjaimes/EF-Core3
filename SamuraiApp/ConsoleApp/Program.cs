2b1425d42ae587c08a43634d45a8fc4a6ab4358b
Manuel Jaimes <4mjaimes@gmail.com>
2020-06-15 15:11:10 -0500

    module 6
    Interacting with related data

--- SamuraiApp/ConsoleApp/Program.cs
+++ SamuraiApp/ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using SamuraiApp.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Data;
 using SamuraiApp.Domain;
 using System;
 using System.Linq;
@@ -7,28 +8,63 @@ namespace ConsoleApp
 {
     class Program
     {
-        private static SamuraiContext context = new SamuraiContext();
+        private static SamuraiContext _context = new SamuraiContext();
 
         private static void Main(string[] args)
         {
-            context.Database.EnsureCreated();
-            GetSamurais("Before Add:");
-            AddSamurai();
-            GetSamurais("After Add:");
+            //context.Database.EnsureCreated();
+            //GetSamurais("Before Add:");
+            InsertMultipleSamurais();
+            //AddSamurai();
+            //GetSamurais("After Add:");
+            //QueryFilters();
+            //RetrieveAndUpdateSamurai();
+            RetrieveAndUpdateMultipleSamurais();
             Console.Write("Press any key...");
             Console.ReadKey();
+        }
+
+        private static void RetrieveAndUpdateMultipleSamurais()
+        {
+            var samurais = _context.Samurais.Skip(1).Take(4).ToList();
+            samurais.ForEach(s => s.Name += "San");
+            _context.SaveChanges();
+        }
+
+        private static void RetrieveAndUpdateSamurai()
+        {
+            var samurai = _context.Samurais.FirstOrDefault();
+            samurai.Name += "San";
+            _context.SaveChanges();
+        }
+
+        private static void QueryFilters()
+        {
+            //var name = "Manuel";
+            //var samurai = _context.Samurais.Where(s => s.Name == name).ToList();
+            var samurai = _context.Samurais.Where(s => EF.Functions.Like(s.Name, "M%")).ToList();
+        }
+
+        private static void InsertMultipleSamurais()
+        {
+            var samurai = new Samurai { Name = "Manuel" };
+            var samurai2 = new Samurai { Name = "Carlos" };
+            var samurai3 = new Samurai { Name = "Evangelina" };
+            var samurai4 = new Samurai { Name = "Fernando" };
+            _context.Samurais.AddRange(samurai, samurai2);
+            _context.SaveChanges();
         }
 
         private static void AddSamurai()
         {
             var samurai = new Samurai { Name = "Manuel" };
-            context.Samurais.Add(samurai);
-            context.SaveChanges();
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
         }
 
         private static void GetSamurais(string text)
         {
-            var samurais = context.Samurais.ToList();
+            var samurais = _context.Samurais.ToList();
             Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
             foreach (var samurai in samurais)
             {
--- SamuraiApp/SamuraiApp.Data/SamuraiContext .cs
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext .cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SamuraiApp.Domain;
 using System;
 
@@ -11,9 +12,22 @@ namespace SamuraiApp.Data
         public DbSet<Clan> Clans { get; set; }
         public DbSet<Battle> Battles { get; set; }
 
+        public static readonly ILoggerFactory ConsoleLoggerFactory
+            = LoggerFactory.Create(builder =>
+           {
+               builder
+                   .AddFilter((category, level) =>
+                       category == DbLoggerCategory.Database.Command.Name
+                       && level == LogLevel.Information)
+                   .AddConsole();
+           });
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\ProjectsV13;Initial Catalog=SamuraiAppData");
+            optionsBuilder
+                .UseLoggerFactory(ConsoleLoggerFactory)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer("Data Source=(localdb)\\ProjectsV13;Initial Catalog=SamuraiAppData");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
