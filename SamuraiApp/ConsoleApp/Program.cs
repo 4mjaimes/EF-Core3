2e4f400f48e136394d1580d7ef818aa58cce7338
Manuel Jaimes <4mjaimes@gmail.com>
2020-06-15 19:44:17 -0500

    module 6

--- SamuraiApp/ConsoleApp/Program.cs
+++ SamuraiApp/ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using SamuraiApp.Data;
 using SamuraiApp.Domain;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ConsoleApp
@@ -14,14 +15,77 @@ namespace ConsoleApp
         {
             //context.Database.EnsureCreated();
             //GetSamurais("Before Add:");
-            InsertMultipleSamurais();
+            //InsertMultipleSamurais();
             //AddSamurai();
             //GetSamurais("After Add:");
             //QueryFilters();
             //RetrieveAndUpdateSamurai();
-            RetrieveAndUpdateMultipleSamurais();
+            //RetrieveAndUpdateMultipleSamurais();
+            //InsertNewSamuraiWithAQuote();
+            //EagerLoadSamuraiWithQuotes();
+            //ProjectSomeProperties();
+            //ProjectSomeSamuraiWithQuotes();
+            //ExplicitLoadQuotes();
+            ModifyingRelatedDataWhenTracked();
             Console.Write("Press any key...");
             Console.ReadKey();
+        }
+
+        private static void ModifyingRelatedDataWhenTracked()
+        {
+            var samurai = _context.Samurais.Include(s => s.Quotes).FirstOrDefault(s => s.Id == 2);
+
+            var quote = samurai.Quotes[0];
+            quote.Text += "Quote Eva";
+            using (var newContext = new SamuraiContext())
+            {
+                //newContext.Quotes.Update(quote);
+                newContext.Entry(quote).State = EntityState.Modified;
+                newContext.SaveChanges();
+            }
+        }
+
+        private static void ExplicitLoadQuotes()
+        {
+            var samurai = _context.Samurais.FirstOrDefault(s => s.Name.Contains("Manuel"));
+            _context.Entry(samurai).Collection(s => s.Quotes).Load();
+            _context.Entry(samurai).Reference(s => s.Horse).Load();
+        }
+
+        private static void ProjectSomeSamuraiWithQuotes()
+        {
+            var samuraiWithHappyQuotes = _context.Samurais
+                .Select(s => new
+                {
+                    Samurai = s,
+                    HappyQuotes = s.Quotes.Where(q => q.Text.Contains("happy"))
+                })
+                .ToList();
+        }
+
+        private static void ProjectSomeProperties()
+        {
+            var someProperties = _context.Samurais.Select(s => new { s.Id, s.Name }).ToList();
+        }
+
+        private static void EagerLoadSamuraiWithQuotes()
+        {
+            var samurai = _context.Samurais.Include(s => s.Quotes).ToList();
+        }
+
+        private static void InsertNewSamuraiWithAQuote()
+        {
+            var samurai = new Samurai
+            {
+                Name = "Manue Shimada",
+                Quotes = new List<Quote>
+                {
+                   new Quote { Text = "I've come to save you"}
+                }
+            };
+
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
         }
 
         private static void RetrieveAndUpdateMultipleSamurais()
@@ -51,7 +115,7 @@ namespace ConsoleApp
             var samurai2 = new Samurai { Name = "Carlos" };
             var samurai3 = new Samurai { Name = "Evangelina" };
             var samurai4 = new Samurai { Name = "Fernando" };
-            _context.Samurais.AddRange(samurai, samurai2);
+            _context.Samurais.AddRange(samurai, samurai2, samurai3, samurai3);
             _context.SaveChanges();
         }
 
