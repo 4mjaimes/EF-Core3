90e080fd853de7d67aa3969f33216ea117b9d91d
Manuel Jaimes <4mjaimes@gmail.com>
2020-06-29 22:26:06 -0500

    module 8
    Finished

--- SamuraiApp/SamuraiApp.Data/SamuraiContext .cs
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext .cs
@@ -7,28 +7,15 @@ namespace SamuraiApp.Data
 {
     public class SamuraiContext : DbContext
     {
+        public SamuraiContext(DbContextOptions<SamuraiContext> options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
         public DbSet<Battle> Battles { get; set; }
 
-        public static readonly ILoggerFactory ConsoleLoggerFactory
-            = LoggerFactory.Create(builder =>
-           {
-               builder
-                   .AddFilter((category, level) =>
-                       category == DbLoggerCategory.Database.Command.Name
-                       && level == LogLevel.Information)
-                   .AddConsole();
-           });
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder
-                .UseLoggerFactory(ConsoleLoggerFactory)
-                .EnableSensitiveDataLogging()
-                .UseSqlServer("Data Source=(localdb)\\ProjectsV13;Initial Catalog=SamuraiAppData");
-        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<SamuraiBattle>()
@@ -36,5 +23,23 @@ namespace SamuraiApp.Data
             modelBuilder.Entity<Horse>()
                 .ToTable("Horses");
         }
+
+        //public static readonly ILoggerFactory ConsoleLoggerFactory
+        //    = LoggerFactory.Create(builder =>
+        //   {
+        //       builder
+        //           .AddFilter((category, level) =>
+        //               category == DbLoggerCategory.Database.Command.Name
+        //               && level == LogLevel.Information)
+        //           .AddConsole();
+        //   });
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder
+        //        .UseLoggerFactory(ConsoleLoggerFactory)
+        //        .EnableSensitiveDataLogging()
+        //        .UseSqlServer("Data Source=(localdb)\\ProjectsV13;Initial Catalog=SamuraiAppData");
+        //}
     }
 }
